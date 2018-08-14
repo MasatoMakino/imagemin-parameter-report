7ca8aef1ed39f23e0b5e54f43d08525e49c0db56
MasatoMakino <unagiinu.g@gmail.com>
2018-08-14 12:00:05 +0900

    画質メニューをjsonに応じて更新。

--- src/js/ImageSlider.js
+++ src/js/ImageSlider.js
@@ -13,19 +13,66 @@ export default class {
     right.src = "img/jpg_photo/mozJpeg/90/l0134.jpg";
 
     this.initSrcPullDown(json);
+    this.initEncoderPullDown(json);
+    this.initQualityPullDown(json);
   }
 
   initSrcPullDown(json) {
     let keys = this.getKeys(json);
-    console.log(keys);
+    this.updatePullDown("srcImg", keys);
   }
 
-  getKeys(jsonObj) {
+  initEncoderPullDown(json) {
+    let keys = this.getKeys(json);
+    const lastPhoto = json[keys[keys.length - 1]];
+
+    keys = this.getKeys(lastPhoto, "object");
+    this.updatePullDown("leftEncoder", keys);
+    this.updatePullDown("rightEncoder", keys);
+  }
+
+  initQualityPullDown(json) {
+    let keys = this.getKeys(json);
+    const lastPhoto = json[keys[keys.length - 1]];
+
+    keys = this.getKeys(lastPhoto, "object");
+    const lastEncoder = lastPhoto[keys[keys.length - 1]];
+
+    keys = this.getKeys(lastEncoder, "object");
+    keys.sort(function(a, b) {
+      let aNum = parseInt(a);
+      let bNum = parseInt(b);
+      if (aNum > bNum) return -1;
+      if (aNum < bNum) return 1;
+      return 0;
+    });
+    if (keys[0] === "100") {
+      keys.shift();
+    }
+    this.updatePullDown("leftQuality", keys);
+    this.updatePullDown("rightQuality", keys);
+  }
+
+  getKeys(jsonObj, typeFilter) {
     let keys = [];
     for (let key in jsonObj) {
       if (!jsonObj.hasOwnProperty(key)) continue;
+      if (typeFilter != null && typeFilter !== typeof jsonObj[key]) continue;
       keys.push(key);
     }
     return keys;
+  }
+
+  generateListFromKeys(keys) {
+    let html = "";
+    for (let key of keys) {
+      html += `<option>${key}</option>`;
+    }
+    return html;
+  }
+
+  updatePullDown(id, keys) {
+    const pullDown = document.getElementById(id);
+    pullDown.innerHTML = this.generateListFromKeys(keys);
   }
 }
