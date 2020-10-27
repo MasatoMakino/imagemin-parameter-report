aeb584a8edfb98f26188a271a943e6b16559474b
MasatoMakino <unagiinu.g@gmail.com>
2020-10-27 17:06:59 +0900

    update : dependencies.

--- webpack.config.script.js
+++ webpack.config.script.js
@@ -1,18 +1,17 @@
 "use strict";
-const path = require("path");
 const webpack = require("webpack");
 
 module.exports = (env, argv) => {
   const config = {
     entry: {
-      main: "./src/js/main.js"
+      main: "./src/js/main.js",
     },
     output: {
       path: `${__dirname}/dist/js`,
-      filename: "[name].js"
+      filename: "[name].js",
     },
     resolve: {
-      extensions: [".js", ".webpack.js", ".web.js", ".js"]
+      extensions: [".js", ".webpack.js", ".web.js", ".js"],
     },
     module: {
       rules: [
@@ -20,19 +19,12 @@ module.exports = (env, argv) => {
           test: /\.js$/,
           exclude: /node_modules/,
           use: {
-            loader: "babel-loader"
-          }
-        }
-      ]
+            loader: "babel-loader",
+          },
+        },
+      ],
     },
-    plugins: [
-      new webpack.ProvidePlugin({
-        // $: "jquery",
-        // jQuery: "jquery",
-        // "window.jQuery": "jquery",
-      })
-    ],
-    devtool: argv.mode === "production" ? "" : "inline-source-map"
+    plugins: [new webpack.ProvidePlugin({})],
   };
   return config;
 };
